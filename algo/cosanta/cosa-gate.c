bcd5119029a8fc6fe45f1120af56f80a602e0896
Decho Kocharin <sourcedecho@outlook.com>
2021-07-27 15:06:12 +0700

    Update cosa-gate.c

--- algo/cosanta/cosa-gate.c
+++ algo/cosanta/cosa-gate.c
@@ -2,15 +2,7 @@
 
 bool register_cosa_algo( algo_gate_t* gate )
 {
-#if defined (COSA_8WAY)
-  gate->scanhash  = (void*)&scanhash_8way_64in_32out;
-  gate->hash      = (void*)&cosa_8way_hash;
-#elif defined (COSA_4WAY)
-  gate->scanhash  = (void*)&scanhash_4way_64in_32out;
-  gate->hash      = (void*)&cosa_4way_hash;
-#else
   gate->hash      = (void*)&cosa_hash;
-#endif
   gate->optimizations = SSE2_OPT | AES_OPT | AVX2_OPT | AVX512_OPT | VAES_OPT | VAES256_OPT;
   return true;
 };

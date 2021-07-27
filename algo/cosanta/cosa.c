39ccd5c56dc15542bd54fab1bdbf680591a3fd32
Decho Kocharin <sourcedecho@outlook.com>
2021-07-27 14:08:41 +0700

    Update cosa.c

--- algo/cosanta/cosa.c
+++ algo/cosanta/cosa.c
@@ -7,8 +7,13 @@
 
 #include "algo/blake/sph_blake.h"
 #include "algo/bmw/sph_bmw.h"
-#include "algo/groestl/sph_groestl.h"
-#include "algo/echo/sph_echo.h"
+#if defined(__AES__)
+  #include "algo/echo/aes_ni/hash_api.h"
+  #include "algo/groestl/aes_ni/hash-groestl.h"
+#else
+  #include "algo/groestl/sph_groestl.h"
+  #include "algo/echo/sph_echo.h"
+#endif 
 #include "algo/jh/sph_jh.h"
 #include "algo/keccak/sph_keccak.h"
 #include "algo/skein/sph_skein.h"
@@ -37,104 +42,125 @@ bool lyra2z_thread_init()
    return lyra2z_matrix;
 }
 
-void cosahash( void *output, const void *input )
+union _cosa_context_overlay
+{
+
+	
+	sph_blake512_context     blake;
+	sph_bmw512_context       bmw;
+#if defined(__AES__)
+    hashState_groestl        groestl;
+    hashState_echo           echo;
+#else
+    sph_groestl512_context   groestl;
+    sph_echo512_context      echo;
+#endif
+	sph_jh512_context        jh;
+	sph_keccak512_context    keccak;
+	sph_skein512_context     skein;
+	sph_luffa512_context     luffa;
+	sph_cubehash512_context  cubehash;
+	sph_shavite512_context   shavite;
+	sph_simd512_context      simd;
+	sph_hamsi512_context     hamsi;
+	sph_fugue512_context     fugue;
+	sph_shabal512_context    shabal;
+	sph_whirlpool_context    whirlpool;
+	sph_sha512_context       sha512;
+	sph_haval256_5_context   haval;
+	sph_gost512_context      gost;
+};
+typedef union _cosa_context_overlay cosa_context_overlay;
+
+void cosa_hash( void *output, const void *input )
 {
 	unsigned char _ALIGN(128) hash[128],hashB[128],hashC[128],hashD[128];
 
-	sph_blake512_context     ctx_blake;
-	sph_bmw512_context       ctx_bmw;
-    sph_groestl512_context   ctx_groestl;
-    sph_echo512_context      ctx_echo;
-	sph_jh512_context        ctx_jh;
-	sph_keccak512_context    ctx_keccak;
-	sph_skein512_context     ctx_skein;
-	sph_luffa512_context     ctx_luffa;
-	sph_cubehash512_context  ctx_cubehash;
-	sph_shavite512_context   ctx_shavite;
-	sph_simd512_context      ctx_simd;
-	sph_hamsi512_context     ctx_hamsi;
-	sph_fugue512_context     ctx_fugue;
-	sph_shabal512_context    ctx_shabal;
-	sph_whirlpool_context    ctx_whirlpool;
-	sph_sha512_context       ctx_sha512;
-	sph_haval256_5_context   ctx_haval;
-	sph_gost512_context      ctx_gost;
+	sph_blake512_init(&ctx.blake);
+	sph_blake512(&ctx.blake, input, 80);
+	sph_blake512_close(&ctx.blake, hash);
 
-	sph_blake512_init(&ctx_blake);
-	sph_blake512(&ctx_blake, input, 80);
-	sph_blake512_close(&ctx_blake, hash);
+	sph_bmw512_init(&ctx.bmw);
+	sph_bmw512(&ctx.bmw, hash, 64);
+	sph_bmw512_close(&ctx.bmw, hash);
 
-	sph_bmw512_init(&ctx_bmw);
-	sph_bmw512(&ctx_bmw, hash, 64);
-	sph_bmw512_close(&ctx_bmw, hash);
+	#if defined(__AES__)
+		init_groestl( &ctx.groestl, 64 );
+		update_and_final_groestl( &ctx.groestl, (char*)hash, (const char*)in, size<<3 );
+	#else
+		sph_groestl512_init(&ctx.groestl);
+		sph_groestl512(&ctx.groestl, in, 64);
+		sph_groestl512_close(&ctx.groestl, hash);
+	#endif
 
-	sph_groestl512_init(&ctx_groestl);
-	sph_groestl512(&ctx_groestl, hash, 64);
-	sph_groestl512_close(&ctx_groestl, hash);
+	sph_skein512_init(&ctx.skein);
+	sph_skein512(&ctx.skein, hash, 64);
+	sph_skein512_close(&ctx.skein, hash);
 
-	sph_skein512_init(&ctx_skein);
-	sph_skein512(&ctx_skein, hash, 64);
-	sph_skein512_close(&ctx_skein, hash);
+	sph_jh512_init(&ctx.jh);
+	sph_jh512(&ctx.jh, hash, 64);
+	sph_jh512_close(&ctx.jh, hash);
 
-	sph_jh512_init(&ctx_jh);
-	sph_jh512(&ctx_jh, hash, 64);
-	sph_jh512_close(&ctx_jh, hash);
+	sph_keccak512_init(&ctx.keccak);
+	sph_keccak512(&ctx.keccak, hash, 64);
+	sph_keccak512_close(&ctx.keccak, hash);
 
-	sph_keccak512_init(&ctx_keccak);
-	sph_keccak512(&ctx_keccak, hash, 64);
-	sph_keccak512_close(&ctx_keccak, hash);
+	sph_luffa512_init(&ctx.luffa);
+	sph_luffa512(&ctx.luffa, hash, 64);
+	sph_luffa512_close(&ctx.luffa, hash);
 
-	sph_luffa512_init(&ctx_luffa);
-	sph_luffa512(&ctx_luffa, hash, 64);
-	sph_luffa512_close(&ctx_luffa, hash);
+	sph_cubehash512_init(&ctx.cubehash);
+	sph_cubehash512(&ctx.cubehash, hash, 64);
+	sph_cubehash512_close(&ctx.cubehash, hash);
 
-	sph_cubehash512_init(&ctx_cubehash);
-	sph_cubehash512(&ctx_cubehash, hash, 64);
-	sph_cubehash512_close(&ctx_cubehash, hash);
+	sph_shavite512_init(&ctx.shavite);
+	sph_shavite512(&ctx.shavite, hash, 64);
+	sph_shavite512_close(&ctx.shavite, hash);
 
-	sph_shavite512_init(&ctx_shavite);
-	sph_shavite512(&ctx_shavite, hash, 64);
-	sph_shavite512_close(&ctx_shavite, hash);
+	sph_simd512_init(&ctx.simd);
+	sph_simd512(&ctx.simd, hash, 64);
+	sph_simd512_close(&ctx.simd, hash);
 
-	sph_simd512_init(&ctx_simd);
-	sph_simd512(&ctx_simd, hash, 64);
-	sph_simd512_close(&ctx_simd, hash);
+	#if defined(__AES__)
+		init_echo( &ctx.echo, 64 );
+        update_final_echo ( &ctx.echo, (BitSequence *)hash,(const BitSequence *)in, size<<3 );
+	#else
+	    sph_echo512_init(&ctx.echo);
+	    sph_echo512(&ctx.echo, in, 64);
+	    sph_echo512_close(&ctx.echo, hash);
+	#endif
+	
+	sph_hamsi512_init(&ctx.hamsi);
+	sph_hamsi512(&ctx.hamsi, hash, 64);
+	sph_hamsi512_close(&ctx.hamsi, hash);
 
-	sph_echo512_init(&ctx_echo);
-	sph_echo512(&ctx_echo, hash, 64);
-	sph_echo512_close(&ctx_echo, hash);
+	sph_fugue512_init(&ctx.fugue);
+	sph_fugue512(&ctx.fugue, hash, 64);
+	sph_fugue512_close(&ctx.fugue, hash);
 
-	sph_hamsi512_init(&ctx_hamsi);
-	sph_hamsi512(&ctx_hamsi, hash, 64);
-	sph_hamsi512_close(&ctx_hamsi, hash);
+	sph_shabal512_init(&ctx.shabal);
+	sph_shabal512(&ctx.shabal, hash, 64);
+	sph_shabal512_close(&ctx.shabal, hash);
 
-	sph_fugue512_init(&ctx_fugue);
-	sph_fugue512(&ctx_fugue, hash, 64);
-	sph_fugue512_close(&ctx_fugue, hash);
+	sph_whirlpool_init(&ctx.whirlpool);
+	sph_whirlpool(&ctx.whirlpool, hash, 64);
+	sph_whirlpool_close(&ctx.whirlpool, hash);
 
-	sph_shabal512_init(&ctx_shabal);
-	sph_shabal512(&ctx_shabal, hash, 64);
-	sph_shabal512_close(&ctx_shabal, hash);
-
-	sph_whirlpool_init(&ctx_whirlpool);
-	sph_whirlpool(&ctx_whirlpool, hash, 64);
-	sph_whirlpool_close(&ctx_whirlpool, hash);
-
-	sph_sha512_init(&ctx_sha512);
-	sph_sha512(&ctx_sha512,(const void*) hash, 64);
-	sph_sha512_close(&ctx_sha512,(void*) hash);
+	sph_sha512_init(&ctx.sha512);
+	sph_sha512(&ctx.sha512,(const void*) hash, 64);
+	sph_sha512_close(&ctx.sha512,(void*) hash);
 
 	memset (hashB,0x0,128);
 	memset (hashC,0x0,128);
 	memset (hashD,0x0,128); 
 
-	sph_haval256_5_init(&ctx_haval);
-	sph_haval256_5(&ctx_haval,(const void*) hash, 64);
-	sph_haval256_5_close(&ctx_haval, hashB);
+	sph_haval256_5_init(&ctx.haval);
+	sph_haval256_5(&ctx.haval,(const void*) hash, 64);
+	sph_haval256_5_close(&ctx.haval, hashB);
 
-	sph_gost512_init(&ctx_gost);
-	sph_gost512(&ctx_gost, (const void*) hashB, 64);
-	sph_gost512_close(&ctx_gost, (void*) hashC);
+	sph_gost512_init(&ctx.gost);
+	sph_gost512(&ctx.gost, (const void*) hashB, 64);
+	sph_gost512_close(&ctx.gost, (void*) hashC);
 
 	LYRA2Z(lyra2z_matrix, hashD, 32, hashC, 80, hashC, 80, 2, 66, 66);
 
@@ -163,7 +189,7 @@ int scanhash_cosa( struct work *work, uint32_t max_nonce,
 
 	do {
 		be32enc(&endiandata[19], nonce);
-		cosahash(hash, endiandata);
+		cosa_hash(hash, endiandata);
 
 		if (hash[7] <= Htarg && fulltest(hash, ptarget)) {
 			work_set_target_ratio(work, hash);

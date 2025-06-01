0b29df84edc75fb294c5e00080673b6c672c47cd
Decho Kocharin <sourcedecho@outlook.com>
2025-06-01 21:22:52 +0700

    Add YespowerEQPAY

--- algo-gate-api.c
+++ algo-gate-api.c
@@ -388,6 +388,7 @@ bool register_algo_gate( int algo, algo_gate_t *gate )
     case ALGO_YESPOWER:     rc = register_yespower_algo      ( gate ); break;
     case ALGO_YESPOWERADVC: rc = register_yespoweradvc_algo  ( gate ); break;
     case ALGO_YESPOWERARWN: rc = register_yespowerarwn_algo  ( gate ); break;
+    case ALGO_YESPOWEREQPAY: rc = register_yespowereqpay_algo( gate ); break;
     case ALGO_YESPOWERMGPC: rc = register_yespowermgpc_algo  ( gate ); break;
     case ALGO_YESPOWERR16:  rc = register_yespowerr16_algo   ( gate ); break;
     case ALGO_YESPOWERSUGAR: rc = register_yespowersugar_algo( gate ); break;
@@ -460,6 +461,7 @@ const char* const algo_alias_map[][2] =
   { "yenten",            "yespowerr16"    },
   { "yespowerADVC",      "yespoweradvc"   },
   { "yespowerarwn",      "yespowerarwn"   },
+  { "yespowerEQPAY",     "yespowereqpay"  },
   { "yespowersugar",     "yespowersugar"  },
   { "yespowerurx",     	 "yespowerurx"    },
   { "ziftr",             "zr5"            },
--- miner.h
+++ miner.h
@@ -690,6 +690,7 @@ enum algos {
         ALGO_YESPOWER,
         ALGO_YESPOWERADVC,
         ALGO_YESPOWERARWN,
+        ALGO_YESPOWEREQPAY,
         ALGO_YESPOWERMGPC,
         ALGO_YESPOWERR16,
         ALGO_YESPOWERSUGAR,
@@ -798,6 +799,7 @@ static const char* const algo_names[] = {
         "yespower",
         "yespoweradvc",
         "yespowerarwn",
+        "yespowereqpay",
         "yespowermgpc",
         "yespowerr16",
         "yespowersugar",
@@ -975,6 +977,7 @@ Options:\n\
                           yespower      Cryply\n\
                           yespoweradvc  AdventureCoin (ADVC)\n\
                           yespowerarwn  ARWN\n\
+                          yespowereqpay EquityPay (EQPAY)\n\
                           yespowermgpc	Magpies\n\
                           yespowerr16   Yenten (YTN)\n\
                           yespowersugar Sugarchain (SUGAR)\n\

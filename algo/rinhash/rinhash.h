672d6ce055adb1df9c065f00f521d02fbbd4f222
Decho Kocharin <sourcedecho@outlook.com>
2025-10-16 23:18:04 +0700

    Revert "Merge pull request #18 from zuun1989/feature/rinhash-opencl"
    This reverts commit 64c95c1b3426dac057e12c5f52abe2c325f732ad, reversing
    changes made to 856fc0925b30117b301f57b183f5e995ca40a5e0.

--- algo/rinhash/rinhash.h
+++ algo/rinhash/rinhash.h
@@ -12,13 +12,6 @@ bool register_rin_algo(algo_gate_t* gate);
 void rinhash(void* output, const void* input);
 int scanhash_rin(int thr_id, struct work *work, uint32_t max_nonce, uint64_t *hashes_done);
 
-// If compiled with OpenCL support, user may enable USE_OPENCL to use GPU path.
-#ifdef USE_OPENCL
-// Provide initialization/teardown declarations if needed by other modules
-int init_rinhash_gpu_wrapper(const char *kernel_path);
-void release_rinhash_gpu_wrapper(void);
-#endif
-
 #ifdef __cplusplus
 }
 #endif
--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ make distclean || echo clean
 rm -f config.status
 ./autogen.sh || echo done
 CFLAGS="-O3 -march=native -Wall" ./configure --with-curl
-make CFLAGS+='-DUSE_OPENCL' LIBS+='-lOpenCL' -j$(nproc)
+make -j $(nproc)
 
 #strip -s cpuminer
